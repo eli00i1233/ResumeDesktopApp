9e500788a978c9de1cd0beb303c0c68bdde9acb7
eli00i1233 <eli00i1233@gmail.com>
2023-07-03 10:23:54 +0400

    Initial commit

---
+++ src/main/java/com/company/resume/config/Config.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.company.resume.config;
+
+import com.mycompany.entity.User;
+
+/**
+ *
+ * @author User
+ */
+public class Config {
+    public static User loggedInUser;
+}
